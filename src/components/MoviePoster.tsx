a8ae05dea3a966ffa579a0b504fb11ea98405b39
josebrusa <josebrusa@outlook.com>
2023-05-21 15:53:45 +0200

    add movie poster and styles card

---
+++ src/components/MoviePoster.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import { Movie } from '../interfaces/movieInterfaces'
+
+
+interface Props  {
+    movie: Movie;
+}
+
+export const MoviePoster = ( { movie }: Props ) => {
+
+    const uri = `https://image.tmdb.org/t/p/w500${ movie.poster_path }`;
+    return (
+        <View style={{
+            width: 250,
+            height: 400,
+
+        }}>
+            <Text>{movie.title}</Text>
+            
+                <Image 
+                    source={{ uri }}
+                    style={ styles.image }
+                />
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    image: {
+        flex: 1,
+        borderRadius:18
+    }
+})
+
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import { ActivityIndicator, Text, View } from 'react-native'
 import { useMovies } from '../hooks/useMovies'
+import { MoviePoster } from '../components/MoviePoster';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 
 export const HomeScreen = () => {
 
     const { peliculasEnCine, isLoadig } = useMovies();
+    const { top } = useSafeAreaInsets();
 
 
     if ( isLoadig ) {
         return (
-            <View style={{ flex: 1, justifyContent:'center', alignItems:'center'}}>
-                <ActivityIndicator size='large' color="#0000ff" />  
+            <View style={{ flex: 1, justifyContent:'center', alignContent:'center'}}>
+                <ActivityIndicator size='large' color="#0000ff" />
             </View>
         )
     }
 
     return (
-        <View>
-            <Text>Home Screen</Text>
+        <View style={{
+            marginTop: top + 20,
+        }}>
+            <MoviePoster
+                movie={ peliculasEnCine[1] }
+            />
         </View>
     )
 }
